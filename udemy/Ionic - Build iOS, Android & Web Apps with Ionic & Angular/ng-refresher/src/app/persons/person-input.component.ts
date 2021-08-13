b1f214b05fa2d7cd844b413754c5d22f0e0ea0f0
masato nishio <var.masatonishio@gmail.com>
2021-08-13 21:43:42 +0900

    28. Local References

--- udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ng-refresher/src/app/persons/person-input.component.ts
+++ udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ng-refresher/src/app/persons/person-input.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./person-input.component.css'],
 })
 export class PersonInputComponent {
-  onCreatePerson() {
-    console.log('created person!');
+  onCreatePerson(personName: string) {
+    console.log('created person! ' + personName);
   }
 }
