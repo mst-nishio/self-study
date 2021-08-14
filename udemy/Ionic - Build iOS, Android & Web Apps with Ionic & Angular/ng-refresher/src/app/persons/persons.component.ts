9ef12bd5e3ca95db98416828d8231fab24f59be0
masato nishio <var.masatonishio@gmail.com>
2021-08-14 13:31:30 +0900

    34. Working with Angular Lifecycle Hooks

--- udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ng-refresher/src/app/persons/persons.component.ts
+++ udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ng-refresher/src/app/persons/persons.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { PersonsService } from './persons.service';
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
 })
-export class PersonsComponent {
+export class PersonsComponent implements OnInit {
   personList: string[];
-  constructor(prsService: PersonsService) {
-    this.personList = prsService.persons;
+
+  constructor(private prsService: PersonsService) {}
+
+  ngOnInit() {
+    this.personList = this.prsService.persons;
   }
 }
