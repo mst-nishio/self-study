eb6840db2dea34df3acb7320b8c6fd5650cc2673
masato nishio <var.masatonishio@gmail.com>
2021-08-14 12:27:25 +0900

    30. Passing Data Around with Custom Events

--- udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ng-refresher/src/app/persons/person-input.component.ts
+++ udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ng-refresher/src/app/persons/person-input.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-person-input',
   templateUrl: './person-input.component.html',
   styleUrls: ['./person-input.component.css'],
 })
 export class PersonInputComponent {
+  @Output() personCreate = new EventEmitter<string>();
   enteredPersonName = '';
 
   onCreatePerson() {
     console.log('created person! ' + this.enteredPersonName);
+    this.personCreate.emit(this.enteredPersonName);
   }
 }
