8b10c8a80728b7a071aba51b6efa84a34f146324
masato nishio <var.masatonishio@gmail.com>
2021-09-05 12:47:49 +0900

    50. constアサーション

---
+++ udemy/ハンズオンで学ぶTypeScript - JavaScript エンジニアのためのTypeScript徹底入門/typescript-for-javascript-developers/src/as-const.ts
@@ -0,0 +1,20 @@
+export {};
+
+let name = "Atsushi";
+
+name = "Ham";
+
+let nickname = "Ham" as const;
+nickname = "Ham";
+
+// const profile = {
+//   name: "Atsushi",
+//   height: 178,
+// };
+let profile = {
+  name: "Atsushi",
+  height: 178,
+} as const;
+
+profile.name = "Ham";
+profile.height = 180;
