343f71a43c21428ec7bb9f7d8d9c05549ef6124e
masato nishio <var.masatonishio@gmail.com>
2021-11-07 14:33:04 +0900

    34. 【修了確認】即時関数

---
+++ udemy/【JS】ガチで学びたい人のためのJavaScriptメカニズム/javascript-essence-tutorial-master/030_930/main.js
@@ -0,0 +1,35 @@
+const calc = (function (num) {
+  let result;
+  function plus(value) {
+    result = num + value;
+    console.log(`${num} + ${value} = ${result}`);
+    num = result;
+  }
+  function minus(value) {
+    result = num - value;
+    console.log(`${num} - ${value} = ${result}`);
+    num = result;
+  }
+  function multiply(value) {
+    result = num * value;
+    console.log(`${num} * ${value} = ${result}`);
+    num = result;
+  }
+  function divide(value) {
+    result = num / value;
+    console.log(`${num} / ${value} = ${result}`);
+    num = result;
+  }
+
+  return {
+    plus,
+    minus,
+    multiply,
+    divide,
+  };
+})(10);
+
+calc.plus(5);
+calc.minus(3);
+calc.multiply(3);
+calc.divide(2);
