43b03cc34d7cab1a6297b76a04efef82383bd61e
masato nishio <var.masatonishio@gmail.com>
2021-11-07 12:58:06 +0900

    27. クロージャー（プライベート変数）

---
+++ udemy/【JS】ガチで学びたい人のためのJavaScriptメカニズム/javascript-essence-tutorial-master/030_050/main.js
@@ -0,0 +1,16 @@
+function incrementFactory() {
+  let num = 0;
+
+  function increment() {
+    num++;
+    console.log(num);
+  }
+  return increment;
+}
+
+const increment = incrementFactory();
+
+increment();
+increment();
+increment();
+increment();
