3baff80c84019db8f4578c79ed3eb772857f0f84
masato nishio <var.masatonishio@gmail.com>
2021-11-14 11:27:49 +0900

    39. 暗黙的な型変換

---
+++ udemy/【JS】ガチで学びたい人のためのJavaScriptメカニズム/javascript-essence-tutorial-master/040_020/main.js
@@ -0,0 +1,28 @@
+function printTypeAndValue(val) {
+  console.log(typeof val, val);
+}
+
+let a = 0;
+
+printTypeAndValue(a);
+
+// let b = "1" + a;
+let b = parseInt("1") + a;
+
+printTypeAndValue(b);
+
+let c = 15 - b;
+
+printTypeAndValue(c);
+
+let d = c - null;
+
+printTypeAndValue(d);
+
+let e = d - true;
+
+printTypeAndValue(e);
+
+let f = e - false;
+
+printTypeAndValue(f);
