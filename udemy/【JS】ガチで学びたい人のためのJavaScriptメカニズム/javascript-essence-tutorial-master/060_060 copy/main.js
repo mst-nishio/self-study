4ba985b3afd6593b2e6920da2323a79b3bce64b2
masato nishio <var.masatonishio@gmail.com>
2021-12-05 13:49:29 +0900

    84. hasOwnPropertyとin

---
+++ udemy/【JS】ガチで学びたい人のためのJavaScriptメカニズム/javascript-essence-tutorial-master/060_060 copy/main.js
@@ -0,0 +1,20 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  // this.hello = () => console.log(`hello ${this.name}`);
+}
+
+// Person.prototype.hello = function () {
+//   console.log(`Person: hello ${this.name}`);
+// };
+
+Object.prototype.hello = function () {
+  console.log(`Object: hello ${this.name}`);
+};
+
+const bob = new Person("Bob", 18);
+// bob.hello();
+
+const result = bob.hasOwnProperty("hello");
+console.log(result);
+console.log("hello" in bob);
