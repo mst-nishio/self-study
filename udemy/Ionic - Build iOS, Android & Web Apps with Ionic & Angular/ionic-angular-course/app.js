ed9ba407f07566fb7127c7a38537fa86c6fb4618
masato nishio <var.masatonishio@gmail.com>
2022-01-08 16:29:28 +0900

    62. Using Controller Components

--- udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ionic-angular-course/app.js
+++ udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ionic-angular-course/app.js
@@ -4,8 +4,10 @@ const cancelBtn = document.querySelector("#btn-cancel");
 const confirmBtn = document.querySelector("#btn-confirm");
 const expensesList = document.querySelector("#expenses-list");
 const totalExpensesOutput = document.querySelector("#total-expenses");
+const alert = document.querySelector("ion-alert");
 
 let totalExPenses = 0;
+let alertPresented = false;
 
 const clear = () => {
   reasonInput.value = "";
@@ -21,7 +23,13 @@ confirmBtn.addEventListener("click", () => {
     enteredAmount <= 0 ||
     enteredAmount.trim().length <= 0
   ) {
-    return;
+    alertPresented = true;
+    alert.message = "Please enter valid reason and amount!";
+    alert.header = "Invalid inputs";
+    alert.buttons = ["OK"];
+
+    document.body.appendChild(alert);
+    return alert.present();
   }
 
   const newItem = document.createElement("ion-item");
