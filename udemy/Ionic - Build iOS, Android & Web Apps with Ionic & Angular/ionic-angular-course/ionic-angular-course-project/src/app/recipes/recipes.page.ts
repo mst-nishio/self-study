bbd35b1aa66f7c435220a63474464fc3292100a1
masato nishio <var.masatonishio@gmail.com>
2022-01-15 13:27:45 +0900

    76. Managing State with Services

--- udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ionic-angular-course/ionic-angular-course-project/src/app/recipes/recipes.page.ts
+++ udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ionic-angular-course/ionic-angular-course-project/src/app/recipes/recipes.page.ts
@@ -1,3 +1,4 @@
+import { RecipesService } from './recipes.service';
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
 @Component({
@@ -6,24 +7,11 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Schnitzel',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/4/4d/Turkey_Schnitzel_with_Spatzle_and_vegetables.JPG',
-      ingredients: ['French Fries', 'Pork Meat', 'Salad'],
-    },
-    {
-      id: 'r2',
-      title: 'Spaghetti',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/a/a9/Barbecue_spaghetti.jpg',
-      ingredients: ['Spaghetti', 'Meat', 'Tomatoes'],
-    },
-  ];
+  recipes: Recipe[];
 
-  constructor() {}
+  constructor(private recipesService: RecipesService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipes = this.recipesService.getAllRecipes();
+  }
 }
---
+++ udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ionic-angular-course/ionic-angular-course-project/src/app/recipes/recipes.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/4/4d/Turkey_Schnitzel_with_Spatzle_and_vegetables.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad'],
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/a/a9/Barbecue_spaghetti.jpg',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes'],
+    },
+  ];
+
+  constructor() {}
+
+  getAllRecipes() {
+    // return [...this.recipes];
+    return this.recipes;
+  }
+
+  getRecipe(recipeId: string) {
+    // return {
+    //   ...this.recipes.find((recipe) => recipe.id === recipeId),
+    // };
+    return this.recipes.find((recipe) => recipe.id === recipeId);
+  }
+}
