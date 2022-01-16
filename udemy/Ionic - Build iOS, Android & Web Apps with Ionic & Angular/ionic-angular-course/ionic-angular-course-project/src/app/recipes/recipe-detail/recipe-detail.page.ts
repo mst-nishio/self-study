0a28086a97a10d9272e99fe2ba7b0d3638e45f87
masato nishio <var.masatonishio@gmail.com>
2022-01-16 14:01:10 +0900

    82. Injecting Ionic Controllers

--- udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ionic-angular-course/ionic-angular-course-project/src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ionic-angular-course/ionic-angular-course-project/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -2,6 +2,7 @@ import { RecipesService } from './../recipes.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,7 +15,8 @@ export class RecipeDetailPage implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private recipesService: RecipesService,
-    private router: Router
+    private router: Router,
+    private alertController: AlertController
   ) {}
 
   ngOnInit() {
@@ -28,7 +30,23 @@ export class RecipeDetailPage implements OnInit {
   }
 
   onDeleteRecipe() {
-    this.recipesService.deleteRecipe(this.loadedRecipe.id);
-    this.router.navigate(['/recipes']);
+    this.alertController
+      .create({
+        header: 'Are you sure?',
+        message: 'Do you really want to delete the recipe?',
+        buttons: [
+          { text: 'Cancel', role: 'cancel' },
+          {
+            text: 'Delete',
+            handler: () => {
+              this.recipesService.deleteRecipe(this.loadedRecipe.id);
+              this.router.navigate(['/recipes']);
+            },
+          },
+        ],
+      })
+      .then((alertEl) => {
+        alertEl.present();
+      });
   }
 }
