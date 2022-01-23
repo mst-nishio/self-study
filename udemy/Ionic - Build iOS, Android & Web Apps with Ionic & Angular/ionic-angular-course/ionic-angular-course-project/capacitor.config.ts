a91bab70520279c100b8da657cf842f0f7d4a4a7
masato nishio <var.masatonishio@gmail.com>
2022-01-23 16:51:34 +0900

    change appId

--- udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ionic-angular-course/ionic-angular-course-project/capacitor.config.ts
+++ udemy/Ionic - Build iOS, Android & Web Apps with Ionic & Angular/ionic-angular-course/ionic-angular-course-project/capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.udemy_course.ionic-angular',
   appName: 'ionic-angular-course-project',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
