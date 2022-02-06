4d18ab28fa3f3e5b6496d1b6c0d5efe2071340d5
masato nishio <var.masatonishio@gmail.com>
2022-02-06 14:45:16 +0900

    105. Ionic Page Caching & Extra Lifecycle Hooks

--- udemy/Ionic-Build_iOS_Android_&_Web_Apps_with_Ionic_&_Angular/ionic-angular-course/ionic-angular-course-project/src/app/recipes/recipes.page.ts
+++ udemy/Ionic-Build_iOS_Android_&_Web_Apps_with_Ionic_&_Angular/ionic-angular-course/ionic-angular-course-project/src/app/recipes/recipes.page.ts
@@ -1,18 +1,39 @@
 import { RecipesService } from './recipes.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit, OnDestroy {
   recipes: Recipe[];
 
   constructor(private recipesService: RecipesService) {}
 
   ngOnInit() {
-    this.recipes = this.recipesService.getAllRecipes();
+    console.log('ionViewWillEnter');
     console.log(this.recipes);
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+    this.recipes = this.recipesService.getAllRecipes();
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
   }
 }
